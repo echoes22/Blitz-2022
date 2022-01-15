cecf61a6fe22b3b8a3d271484099c67c181f6d7b
Christophe Nadeau <christophe.nadeau.2@ulaval.ca>
2022-01-14 22:08:07 -0500

    wip

--- starterkits/python/my_lib/models.py
+++ starterkits/python/my_lib/models.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Union
+from typing import Union, List
 
 from game_message import Position, Diamond, Unit
 
@@ -15,3 +15,23 @@ class Target:
     target_type: TargetType
     target: Union[Diamond, Unit]
     position: Position
+
+
+@dataclass
+class Map:
+    grid: List[Position]
+    targets: List[Target]
+
+    def set_target(self, targets: List[Target]):
+        self.targets = [target for target in targets if self.target_is_in_map(target)]
+
+    def target_is_in_grid(self, target: Target):
+        for poss in self.grid:
+            if target.position.x == poss.x and target.position.y == poss.y:
+                return True
+        return False
+
+#
+# @dataclass
+# class Game:
+#     map: List[Map]
--- starterkits/python/my_lib/pathfinder_manager.py
+++ starterkits/python/my_lib/pathfinder_manager.py
@@ -5,10 +5,6 @@ from game_message import Tick, Position
 from my_lib.models import Target
 
 
-def _get_distance(pos1: Position, pos2: Position):
-    return sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2)
-
-
 class PathFinderManager:
     def __init__(self, tick: Optional[Tick] = None):
         self._tick: Optional[Tick] = tick
@@ -16,9 +12,16 @@ class PathFinderManager:
     def set_tick(self, tick: Tick):
         self._tick = tick
 
-    def get_nearest_targets(self, origin: Position, targets: List[Target]) -> List[Target]:
-        target_result = [for target in targets]
+    def get_nearest_target(self, origin: Position, targets: List[Target]) -> Target:
+        min_distance = 99999
+        nearest_target = None
         for target in targets:
-            distance = _get_distance(unit_pos, diamond.position)
-            if distance <= closest_diamond["distance"]:
-                closest_diamond = {"diamond": diamond, "distance": distance}
+            distance = self._get_distance(origin, target.position)
+            if distance <= min_distance:
+                nearest_target = target
+
+        return nearest_target
+
+    @staticmethod
+    def _get_distance(pos1: Position, pos2: Position):
+        return sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2)
