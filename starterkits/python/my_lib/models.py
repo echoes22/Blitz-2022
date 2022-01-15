4b84070a0566b8a7dc8474705b37ca63b4d431ea
Christophe Nadeau <christophe.nadeau.2@ulaval.ca>
2022-01-14 22:29:20 -0500

    wip

--- starterkits/python/my_lib/models.py
+++ starterkits/python/my_lib/models.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Union, List
+from typing import Union
 
 from game_message import Position, Diamond, Unit
 
@@ -15,23 +15,3 @@ class Target:
     target_type: TargetType
     target: Union[Diamond, Unit]
     position: Position
-
-
-@dataclass
-class Map:
-    grid: List[Position]
-    targets: List[Target]
-
-    def set_target(self, targets: List[Target]):
-        self.targets = [target for target in targets if self.target_is_in_map(target)]
-
-    def target_is_in_grid(self, target: Target):
-        for poss in self.grid:
-            if target.position.x == poss.x and target.position.y == poss.y:
-                return True
-        return False
-
-#
-# @dataclass
-# class Game:
-#     map: List[Map]
